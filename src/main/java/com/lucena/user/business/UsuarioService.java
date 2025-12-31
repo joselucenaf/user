8872bec3280874a5a2182f75aa88f242cca29527
Jose Lucena <joselucenafn@gmail.com>
2025-12-31 03:05:00 -0300

    inclusão do put de usuário

--- src/main/java/com/lucena/user/business/UsuarioService.java
+++ src/main/java/com/lucena/user/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.lucena.user.infrastructure.entity.Usuario;
 import com.lucena.user.infrastructure.exceptions.ConflictException;
 import com.lucena.user.infrastructure.exceptions.ResourceNotFoundException;
 import com.lucena.user.infrastructure.repository.UsuarioRepository;
+import com.lucena.user.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
@@ -45,5 +47,22 @@ public class UsuarioService {
     }
     public void deletaUsuarioPorEmail(String email){
         usuarioRepository.deleteByEmail(email);
+    }
+    public UsuarioDTO atualizaDadosUsuario(String token, UsuarioDTO dto){
+        //Busca o email do usuário através do Token(sem precisar passar o email)
+        String email = jwtUtil.extrairEmailToken(token.substring(7));
+
+        //criptografia de senha
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
+
+        //Busca os dados do usuário no BD
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email).orElseThrow(() ->
+                new ResourceNotFoundException("Email não encontrado"));
+
+        //Mescla os dados que recebe na requisição - DTO com os dados do BD
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+
+        //Salva os dados do usuário convertido e depois pega o retorno e converte para UsuarioDTO
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
     }
 }
--- src/main/java/com/lucena/user/controller/UsuarioController.java
+++ src/main/java/com/lucena/user/controller/UsuarioController.java
@@ -44,4 +44,10 @@ public class UsuarioController {
         usuarioService.deletaUsuarioPorEmail(email);
         return ResponseEntity.ok().build();
     }
+
+    @PutMapping
+    public ResponseEntity<UsuarioDTO> atualizDadoUsuario(@RequestBody UsuarioDTO dto,
+                                                          @RequestHeader("Authorization")String token){
+        return ResponseEntity.ok(usuarioService.atualizaDadosUsuario(token, dto));
+    }
 }
