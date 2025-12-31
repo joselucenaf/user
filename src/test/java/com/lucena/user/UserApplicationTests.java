c433f3e6567abd24565b72f5b78e85d69d5ed783
Jose Lucena <joselucenafn@gmail.com>
2025-12-30 22:05:55 -0300

    commit inicial

---
+++ src/test/java/com/lucena/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucena.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
